dafdc82214d0200faa0aa54ae0dbccbcb1bac28d
JoeGopher <john19rus@gmail.com>
2022-06-21 10:30:19 +0700

    Request-Response by browser

--- internal/app/TransactionAPI/server.go
+++ internal/app/TransactionAPI/server.go
@@ -3,6 +3,9 @@ package TransactionAPI
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 func Start(config *Config) error {
@@ -11,15 +14,45 @@ func Start(config *Config) error {
 		UserID: 1,
 	}
 	DBServer = transactionImplementation
-	transactionStatus, err := DBServer.CheckStatusById(1)
+	transactionstatus, err := DBServer.CheckStatusById(1)
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(transactionStatus)
+	fmt.Println(transactionstatus)
 	allPaymentsByUserId, err := DBServer.GetAllPaymentsByUserId(1)
 	if err != nil {
 		log.Println(err)
 	}
 	fmt.Println(allPaymentsByUserId)
-	return nil
+
+	// http server
+
+	http.HandleFunc("/getStatus", getStatusById)
+
+	return http.ListenAndServe(":9000", nil)
+
+	//return nil
+}
+
+func getStatusById(w http.ResponseWriter, req *http.Request) {
+	splitRoute := strings.Split(req.URL.String(), "?id=")
+	id, err := strconv.Atoi(splitRoute[1])
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Value of id: %d\n", id)
+	var DBServer DbServerInterface
+	transactionImplenemtation := Transaction{
+		UserID: id,
+	}
+	DBServer = transactionImplenemtation
+	status, err := DBServer.CheckStatusById(id)
+	if err != nil {
+		log.Println(err)
+	}
+	b, err := fmt.Fprint(w, status)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(b)
 }
